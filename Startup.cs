1a7d24f999d0899d11c9be83d51f4e996169709a
Ricardo <rlopesgoes@hotmail.com>
2020-08-09 19:29:41 -0300

    -.Net core 3.1 support

--- Startup.cs
+++ Startup.cs
@@ -37,7 +37,8 @@ namespace XMLEditor
                 options.Cookie.IsEssential = true;
             });
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest).AddMvcOptions(options => options.EnableEndpointRouting =false);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
