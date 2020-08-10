b98e416f8d177596fab37a9f0fb1b19b48780917
Ricardo <rlopesgoes@hotmail.com>
2020-08-09 21:14:35 -0300

    Corrected mysql dump file

--- trans.sql
+++ trans.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 5.6.17, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.21, for Linux (x86_64)
 --
--- Host: localhost    Database: translations
+-- Host: lapy.com.br    Database: translations
 -- ------------------------------------------------------
 -- Server version	5.7.24
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,11 +21,30 @@
 
 DROP TABLE IF EXISTS `Keys`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `Keys` (
   `Key` varchar(500) NOT NULL,
   PRIMARY KEY (`Key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `changelog`
+--
+
+DROP TABLE IF EXISTS `changelog`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `changelog` (
+  `ChangeId` int(11) NOT NULL AUTO_INCREMENT,
+  `UserId` int(11) DEFAULT NULL,
+  `ChangedKey` varchar(45) DEFAULT NULL,
+  `ChangedLanguage` varchar(45) DEFAULT NULL,
+  `OldValue` varchar(1500) DEFAULT NULL,
+  `NewValue` varchar(1500) DEFAULT NULL,
+  `TimeStamp` datetime DEFAULT NULL,
+  PRIMARY KEY (`ChangeId`)
+) ENGINE=InnoDB AUTO_INCREMENT=4148 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -34,12 +53,31 @@ CREATE TABLE `Keys` (
 
 DROP TABLE IF EXISTS `translations`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `translations` (
   `Key` varchar(255) NOT NULL,
-  `Lng` varchar(6) NOT NULL,
-  `Value` varchar(1000) DEFAULT NULL,
+  `Lng` varchar(8) NOT NULL,
+  `Value` varchar(1500) DEFAULT NULL,
   PRIMARY KEY (`Key`,`Lng`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `UserId` int(11) NOT NULL,
+  `Login` varchar(45) DEFAULT NULL,
+  `Name` varchar(45) DEFAULT NULL,
+  `LastName` varchar(45) DEFAULT NULL,
+  `Password` varchar(45) DEFAULT NULL,
+  `EnabledLanguages` varchar(45) DEFAULT 'en-us',
+  `EditableLanguages` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`UserId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -52,4 +90,4 @@ CREATE TABLE `translations` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-11-11 20:16:04
+-- Dump completed on 2020-08-09 20:38:00
